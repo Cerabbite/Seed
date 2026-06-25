c8f96f700dcd40260b77581ce5d9ea0b059fc7fe
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2026-06-25 22:55:14 +0200

    Remove " from commit hash

--- build.bash
+++ build.bash
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# Exit immediately if a command fails
 set -e
 
-# 1. Get the Git Hash
 COMMIT_HASH=$(git rev-parse --short HEAD)
 if [[ -n $(git status --porcelain) ]]; then
-    COMMIT_HASH="$COMMIT_HASH-dirty"
+    COMMIT_HASH=$COMMIT_HASH-dirty
 fi
 
 echo "--- Building Seed (Hash: $COMMIT_HASH) ---"
 
 mkdir -p bin
 
-# 2. Build the project
-# -o:speed tells Odin to optimize for performance
-# -define:COMMIT_HASH injects our dynamic git hash
-# -extra-linker-flags passes the necessary system libraries
 odin build seed \
     -out:bin/seed \
     -o:speed \
@@ -25,5 +19,4 @@ odin build seed \
 
 echo "--- Build Complete: ./seed ---"
 
-# 3. Size check (optional)
 ls -lh seed
